b33d0ed791c6f3e34f768a2a61f149d85d043221
T.L <tony1016@gmail.com>
2016-05-19 15:59:15 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # BurpLogFilter
-A python program to filter Burp Suite log file.
+一个python3写的，用于过滤BurpSuite日志的小程序
+
+A python3 program to filter BurpSuite log file.
+
+# WHY？
+
+为什么要写这个程序呢？强大的SqlMap支持使用BurpSuite的日志进行批量分析，但是BurpSuite的日志记录了所有走代理的流量，包括静态资源啊，重复的提交啊，这些都会影响SqlMap的分析效率。于是打算写一个小程序，可以做到：
+
+- 可以按照域名过滤请求
+- 可以自动过滤静态资源请求
+- 可以自动按照模式过滤URL，即相同URL和参数的请求，只会留其一（参数值对于SqlMap没有什么作用）
+
+Why I wrote this program?The powerful SqlMap accepts a BurpSuite log file to make batch anaylze,but the log of BurpSuite record everything,includes static resources,duplicated submits,which will reduce the efficiency of the analyze.So I wrote this utility to make:
+
+- can filter with a hostname
+- can filter static resources automatic
+- can filter duplicated submits according to the url and params(the value of params is useless for SqlMap analyze)
+
+
+
+
+# USAGE
+
+### 1.勾选BurpSuite输出日志(check the logging option)
+
+![burpsuite](https://github.com/tony1016/BurpLogFilter/raw/master/res/burpsuite.png)
+
+### 2.使用burplogfilter.py过滤日志(use burplogfilter.py to filter log file)
+
+```sh
+Usage: python3 burplogfilter.py [options]
+
+Options:
+  -h                                  Show this showHelp
+  -f filepath                         The BurpSuite log to analyze
+  --host keyword, --host=keyword      Host name filter
+  -v                                  Show debug message
+
+Examples:
+  python3 burplogfilter.py -f /tmp/burp.log --host='google.com' > burp-proxy.log
+```
+
+### 3.使用SqlMap批量分析日志(Use SqlMap to batch analyze log)
+
+```sh
+sqlmap -l burp-proxy.log --batch -smart
+```
+
+### 4.查看分析结果(Check result)
+
+```sh
+ls /usr/local/Cellar/sqlmap/0.9_1/libexec/output/
+```
+
---
+++ burplogfilter.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+import getopt
+import sys
+import re
+
+DEBUG=False
+url_param_patterns=[];
+
+def main():
+	global DEBUG
+
+	try:
+		options,args = getopt.getopt(sys.argv[1:],"f:hv",["host="])
+	except getopt.GetoptError:
+		print("[WARNING] error, to see help message of options run with '-h'")
+		sys.exit()
+
+	if ('-v', '') in options:
+		DEBUG=True
+
+	filename=None
+	host=None
+
+	for opt,arg in options:
+		if opt == "-f":
+			filename=arg
+		if opt == "--host":
+			host=arg
+		if opt == "-h":
+			showHelp()
+			return
+
+	blocks=scrapBlocks(filename)
+
+	filteredBlocks=[]
+	for block in blocks:
+		if isBlockUseful(block,host) :
+			filteredBlocks.append(block)
+
+	for block in filteredBlocks:
+		outputBlock(block)
+
+def scrapBlocks(filename):
+	global DEBUG
+
+	if DEBUG:
+		print("Try to anayze file %s"%filename)
+
+	blocks=None
+	with open(filename) as file:
+		content=file.read();
+		blocks=content.split("\n\n\n\n")
+		if DEBUG:
+			print("The file contains %s block(s)"%len(blocks))
+
+	return blocks
+	
+
+def isBlockUseful(block,host,isFilterStaticResource=True):
+	global url_param_patterns
+
+	# 过滤静态资源
+	for line in block.split("\n"):
+		if re.match("^GET",line):
+			if isFilterStaticResource and line.split(" ")[1].split("?")[0].split(".")[-1] in (("bmp","bz2","css","doc","eot","flv","gif","gz","ico","jpeg","jpg","js","less","mp[34]","pdf","png","rar","rtf","swf","tar","tgz","txt","wav","woff","xml","zip")):
+				if DEBUG:
+					print("[DEBUG] Filter this static resource url %s"%line.split(" ")[1])
+				return False
+
+	# 过滤Host
+	for line in block.split("\n"):
+		if re.match("^Host:",line):
+			if host not in line[6:]:
+				if DEBUG:
+					print("[DEBUG] Filter this host %s"%line[6:])
+				return False
+
+	# 过滤URL模式
+	for line in block.split("\n"):
+		if re.match("^GET",line) or re.match("^POST",line):
+			url=line.split(" ")[1].split("?")[0]
+			params=""
+			if "?" in line:
+				params=line.split(" ")[1].split("?")[1]
+			
+
+			pattern=generatePattern(url,params)
+			if pattern in url_param_patterns:
+				if DEBUG:
+					print("[DEBUG] Pattern %s exists"%pattern)
+				return False
+			else:
+				url_param_patterns.append(pattern)
+				if DEBUG:
+					print("[DEBUG] Add new pattern %s"%pattern)
+		
+	return True
+
+def generatePattern(url,params):
+	pattern=[]
+	pattern.append(url)
+	paramKeys=[]
+	for item in params.split("&"):
+		paramKeys.append(item.split("=")[0])
+	paramKeys.sort()
+	pattern.extend(paramKeys)
+	return pattern
+
+
+def outputBlock(block):
+	print("\n"+block+"\n\n\n\n")
+
+def showHelp():
+	print("\n+-----------------------------+")
+	print("|  burplogfilter.py v0.1.0    |")
+	print("|  Tony Lee                   |")
+	print("|  tony1016@gmail.com         |")
+	print("+-----------------------------+\n")
+	print("Usage: python3 burplogfilter.py [options]\n")
+	print("Options:")
+	print("  -h                                  Show this showHelp")
+	print("  -f filepath                         The BurpSuite log to analyze")
+	print("  --host keyword, --host=keyword      Host name filter")
+	print("  -v                                  Show debug message")
+	print("\nExamples:")
+	print("  python3 burplogfilter.py -f /tmp/burp.log --host='google.com'")
+	print("\n[!] to see help message of options run with '-h'")
+
+if __name__ == '__main__':
+	main()
